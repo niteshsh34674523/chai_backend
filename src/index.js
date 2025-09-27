aaf2c1cbbb379c66c5717006352016ad2d9f020e
Nitesh Sharma <niteshsh34@gmail.com>
2025-09-27 23:01:54 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant";
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    }catch(error){
+        console.error("ERROR:- ", error)
+        throw error
+    }
+})()
